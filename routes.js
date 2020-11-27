fa53944614f5ae6f69ade385bc0cbf8e330ea254
Glitch (hello-express) <none>
2020-11-27 16:59:15 +0000

    🍐😀 Checkpoint ./routes.js:25100577/165

--- routes.js
+++ routes.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('ninjas',function());
+router.post('ninjas',function(req,res){
+  console.log(req.body);
+  res.send({type:'POST'});
+});
+
+router.get('ninjas',function(req,res){
+  console.log(req.body);
+  res.send({type:'POST'});
+});
